53bdc7d9dc2157d44119ce7bdae398a8bd3e5900
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-12-15 11:36:13 -0800

    about to add custom layout to shinyApp

---
+++ shinyApp/cyjView.R
@@ -0,0 +1,143 @@
+library(cyjShiny)
+#----------------------------------------------------------------------------------------------------
+readHamidsTCellNetwork <- function()
+{
+   tbl <- read.table("data/cytoscapeEdgeAnnot_11July2017.txt", sep="\t", header=TRUE, as.is=TRUE)
+   tbl.xtab <- as.data.frame(table(c(tbl$Source, tbl$Target)))
+   tbl.xtab <- tbl.xtab[order(tbl.xtab$Freq, decreasing=TRUE),]
+   head(tbl.xtab)
+      # DNMT3A is the most connected gene, a methyl transferase.
+      # extended location: chr12:3,751,728-3,970,655  (218kb)
+
+   colnames(tbl)[1:2] <- c("source", "target")
+   tbl
+
+} # readHamidsTCellNetwork
+#----------------------------------------------------------------------------------------------------
+styleList <- c("", "Yeast-Galactose"="yeastGalactoseStyle.js", "style.js")
+condition <- c("", "gal1RGexp", "gal4RGexp", "gal80Rexp")
+tbl.edges <- readHamidsTCellNetwork()
+tbl.dumb <- data.frame(source="A", target="B", interaction="simple", stringsAsFactors=FALSE)
+tbl.dumb <- tbl.edges[1:3, c("source", "target", "interaction")]
+graph <- dataFramesToJSON(tbl.edges)
+all.nodes <- sort(unique(c(tbl.edges$source, tbl.edges$target)))
+#----------------------------------------------------------------------------------------------------
+ui = shinyUI(fluidPage(
+
+  #tags$head(
+  #        tags$link(rel = "stylesheet", type = "text/css",
+  #                  href = "http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css")),
+
+  sidebarLayout(
+      sidebarPanel(
+          selectInput("loadStyleFile", "Select Style: ", choices=styleList),
+          selectInput("doLayout", "Select Layout:",
+                      choices=c("",
+                                "cose",
+                                "cola",
+                                "circle",
+                                "concentric",
+                                "breadthfirst",
+                                "grid",
+                                "random",
+                                "dagre",
+                                "cose-bilkent")),
+
+          selectInput("setNodeAttributes", "Select Condition:", choices=condition),
+          selectInput("selectName", "Select Node by ID:", choices = c("", all.nodes)),
+          actionButton("sfn", "Select First Neighbor"),
+          actionButton("fit", "Fit Graph"),
+          actionButton("fitSelected", "Fit Selected"),
+          actionButton("clearSelection", "Unselect Nodes"),
+          HTML("<br>"),
+          actionButton("loopConditions", "Loop Conditions"),
+          HTML("<br>"),
+          actionButton("getSelectedNodes", "Get Selected Nodes"),
+          HTML("<br><br>"),
+          htmlOutput("selectedNodesDisplay"),
+          width=3
+      ),
+      mainPanel(cyjShinyOutput('cyjShiny', height=800), width=9
+      )
+  ) # sidebarLayout
+))
+#----------------------------------------------------------------------------------------------------
+server = function(input, output, session)
+{
+    observeEvent(input$fit, ignoreInit=TRUE, {
+       fit(session, 80)
+       })
+
+    observeEvent(input$setNodeAttributes, ignoreInit=TRUE, {
+       attribute <- "lfc"
+       expression.vector <- switch(input$setNodeAttributes,
+                                   "gal1RGexp" = tbl.mrna$gal1RGexp,
+                                   "gal4RGexp" = tbl.mrna$gal4RGexp,
+                                   "gal80Rexp" = tbl.mrna$gal80Rexp)
+       setNodeAttributes(session, attributeName=attribute, nodes=yeastGalactodeNodeIDs, values=expression.vector)
+       })
+
+    observeEvent(input$loadStyleFile,  ignoreInit=TRUE, {
+      showModal(modalDialog("load style file"))
+      #if(input$loadStyleFile != "")
+      #   tryCatch({
+      #      loadStyleFile(input$loadStyleFile)
+      #      }, warning=function(e){
+      #           printf("loadStyleFile warning: %s", e$message)
+      #           showModal(modalDialog(title="cyjShiny error", e$message))
+      #           }
+      #         )
+       })
+
+    observeEvent(input$doLayout,  ignoreInit=TRUE,{
+       strategy <- input$doLayout
+       doLayout(session, strategy)
+       })
+
+    observeEvent(input$selectName,  ignoreInit=TRUE,{
+       session$sendCustomMessage(type="selectNodes", message=list(input$selectName))
+       })
+
+    observeEvent(input$sfn,  ignoreInit=TRUE,{
+       session$sendCustomMessage(type="sfn", message=list())
+       })
+
+    observeEvent(input$fitSelected,  ignoreInit=TRUE,{
+       fitSelected(session, 100)
+       })
+
+    observeEvent(input$getSelectedNodes, ignoreInit=TRUE, {
+       output$selectedNodesDisplay <- renderText({" "})
+       getSelectedNodes(session)
+       })
+
+    observeEvent(input$clearSelection,  ignoreInit=TRUE, {
+       session$sendCustomMessage(type="clearSelection", message=list())
+       })
+
+    observeEvent(input$loopConditions, ignoreInit=TRUE, {
+       condition.names <- c("gal1RGexp", "gal4RGexp", "gal80Rexp")
+       for(condition.name in condition.names){
+          expression.vector <- tbl.mrna[, condition.name]
+          setNodeAttributes(session, attributeName="lfc", nodes=yeastGalactodeNodeIDs, values=expression.vector)
+          Sys.sleep(1)
+          } # for condition.name
+       updateSelectInput(session, "setNodeAttributes", selected="gal1RGexp")
+       })
+
+    observeEvent(input$selectedNodes, {
+       newNodes <- input$selectedNodes;
+       output$selectedNodesDisplay <- renderText({
+          paste(newNodes)
+          })
+       })
+
+    output$value <- renderPrint({input$action})
+
+    output$cyjShiny <- renderCyjShiny(
+       cyjShiny(graph, "cola", "yeastGalactoseStyle.js")
+       )
+
+} # server
+#----------------------------------------------------------------------------------------------------
+app <- shinyApp(ui = ui, server = server)
--- style.js
+++ style.js
@@ -1,97 +1,38 @@
-vizmap = [
+[
 
-   {selector: "node", css: {
-      "shape": "ellipse",
-      "text-valign":"center",
-      "text-halign":"center",
-      "content": "data(label)",
-      "background-color": "#FFFFFF",
-      "border-color":"black","border-width":"1px",
-      "width": "mapData(degree, 0.0, 100.0, 20.0, 200.0)",
-      "height":"mapData(degree, 0.0, 100.0, 20.0, 200.0)",
-      "font-size":"32px"}},
+  {"selector": "node",
+     "css": {
+        "text-valign": "center",
+        "text-halign": "center",
+        "border-color": "red",
+        "background-color": "mapData(assay, 0.0, 40, white, red)",
+        "border-width": 1,
+        "label": "data(id)",
+        "height": 60,  // defaults
+        "width": 60
+        }},
 
+   {"selector": "edge",
+      "css": {
+        "width": "1px",
+        "line-color": "blue",
+        "target-arrow-shape": "triangle",
+        "target-arrow-color": "black",
+        "curve-style": "bezier"
+        }},
 
-   {selector: "node[type='info']", css: {
-       "shape": "roundrectangle",
-       "font-size": "72px",
-       "width": "360px",
-       "height": "120px",
-       "border-width": "3px",
-       "background-color": "beige"
-       }},
-
-   {selector: "node[type='regulatoryRegion']", css: {
-       "shape": "roundrectangle",
-       "width": "40px",
-       "height": "30px",
-       "font-size": "8px",
-       "background-color": "beige"
-       }},
-
-   {selector: "node[type='TF'][pearsonCoeff>0]", css: {
-      "shape": "ellipse",
-      "background-color": "mapData(pearsonCoeff, 0, 1.0, white, red)",
-      "width": "mapData(rfScore, 0.0, 50.0, 20.0, 200.0)",
-      "height":"mapData(rfScore, 0.0, 50.0, 20.0, 200.0)"
-       }},
-
-   {selector: "node[type='targetGene']", css: {
-      "shape": "octagon",
-      "background-color": "lightBlue",
-      "width": 100,
-      "height": 100
-       }},
-
-   {selector: "node[type='TF'][pearsonCoeff<=0]", css: {
-      "shape": "ellipse",
-      "background-color": "mapData(pearsonCoeff, -1.0, 0, green, white)",
-      "width": "mapData(rfScore, 0.0, 50.0, 20.0, 200.0)",
-      "height":"mapData(rfScore, 0.0, 50.0, 20.0, 200.0)"
-       }},
-
-   {selector: "node[type='tf']", css: {
-       "shape": "rectangle"
-       }},
-
-
-   {selector: "node[score=1]", css: {
-       "background-color": "#AAFFAA"
-       }},
-
-   {selector: "node[score=2]", css: {
-       "background-color": "#DDDDFF"
-       }},
-
-   {selector: "node[score=3]", css: {
-       "background-color": "#FF2222"
-       }},
-
-   {selector: 'edge', css: {
-      "line-color": "rgb(200, 200, 200)",
-      "target-arrow-shape": "triangle",
-      "target-arrow-color": "rgb(0, 0, 0)",
-      "curve-style": "bezier",
-      "width": "2px"
+   {"selector": "edge:selected", "css": {
+      "line-color": "rgb(200, 200, 0)",
+      "overlay-opacity": 0.2,
+      "overlay-color": "gray"
       }},
 
-
-   {selector: 'edge:selected', css: {
-      "line-color": "red",
-      "target-arrow-shape": "triangle",
-      "target-arrow-color": "rgb(0, 0, 0)",
-       width: "5px",
-      'curve-style': 'bezier'
-      }},
-
-   {selector:"node:selected", css: {
+   {"selector":"node:selected", "css": {
        "text-valign":"center",
        "text-halign":"center",
        "border-color": "black",
-       "content": "data(label)",
        "border-width": "3px",
        "overlay-opacity": 0.2,
        "overlay-color": "gray"
         }}
-
-   ];
+   ]
